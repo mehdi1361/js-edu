2e1c3819875df96269ce17fa169f2aaca217fee1
m.mousavi <mhd.mosavi@gmail.com>
2020-10-25 03:22:26 +0330

    property and

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -37,9 +37,19 @@ class ProductItem {
 
 class ShopingCart {
     items = [];
+    set cartItems(value) {
+        this.items = value;
+        this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(2)}</h2>`;
+
+    }
+    get totalAmount() {
+        const sum = this.items.reduce((prevValue, curItem) => prevValue + curItem.price, 0);
+        return sum;
+    }
     addProduct(product) {
-        this.items.push(product);
-        this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`
+        const updateItems = [...this.items];
+        updateItems.push(product);
+        this.cartItems = updateItems;
     }
     render() {
         const cartEl = document.createElement('section');
